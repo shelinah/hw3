150ef285fc1db6ee18259e2c2a04fa0bf51de595
shelinah <shelina.hsieh@kellogg.northwestern.edu>
2026-06-01 22:09:40 +0000

    hw3 code

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,20 @@
+class PlacesController < ApplicationController
+  def index
+    @places = Place.all
+  end
+
+  def show
+    @place = Place.find_by({ "id" => params["id"] })
+    @entries = @place.entries
+  end
+
+  def new
+  end
+
+  def create
+    @place = Place.new
+    @place["name"] = params["name"]
+    @place.save
+    redirect_to("/places")
+  end
+end
